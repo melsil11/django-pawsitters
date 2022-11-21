fd5a7db543cd47583781118bacd9b0e93323bf0c
sam-phillips21 <phillips.sam@gmail.com>
2022-11-21 13:32:26 -0700

    petowner model, views, and urls

---
+++ api/models/pet_owner.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class PetOwner(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    pet_type = models.CharField(max_length=100)
+    pet_name = models.CharField(max_length=100)
+    
+    owner = models.ForeignKey(
+      get_user_model(),
+      on_delete=models.CASCADE)
+    images = models.TextField()
+    
+    # upload = models.ImageField(upload_to ='uploads/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.pet_type} {self.pet_name} "
+
