b9e8dbcf57cd2c0e462fd696ddef1ac3e27b7818
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-21 15:26:37 -0500

    adds pet sitter model, views

---
+++ api/models/pet_sitters.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from .pet_types import PetTypes
+
+# Create your models here.
+
+
+class PetSitter(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(default=18)
+    pet_types = models.ManyToManyField(PetTypes)
+    rate = models.IntegerField()
+    date = models.DateField
+    time = models.TimeField
+    medicine = models.Boolean(default=False)
+    color = models.CharField(max_length=100)
+    image = models.TextField(null=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        # This must return a string
+        return f"Pet Sitter: {self.first_name} {self.first_name}"
+
