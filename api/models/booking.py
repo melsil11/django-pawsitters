19b6456c279383eb2f38f7dd265fdf8f8e3e695f
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-25 16:55:06 -0500

    note re-added

--- api/models/booking.py
+++ api/models/booking.py
@@ -32,7 +32,7 @@ class Booking(models.Model):
 
     def __str__(self):
         # This must return a string
-        return f"{self.pet_sitter} will be pet-sitting for {self.pet_owner} on {self.start_day} at {self.start_time}."
+        return f"{self.pet_sitter} will be pet-sitting for {self.pet_owner} on {self.start_day} at {self.start_time}.{self.note}"
 
     def as_dict(self):
         """Returns dictionary version of Booking models"""
@@ -40,6 +40,7 @@ class Booking(models.Model):
             'id': self.id,
             'pet owner': self.pet_owner,
             'pet-sitter': self.pet_sitter,
+            'note': self.note,
             'scheduled day for start': self.start_day,
             'scheduled day for end': self.end_day,
             'scheduled start time': self.start_time,
