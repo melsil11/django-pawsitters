bf36664eadc01813aac1a6bddf40f3ea3bd15169
Angel Zhou <angel.zhou1016@gmail.com>
2022-11-30 11:17:25 -0500

    removes unnecessary curl scripts

--- pawsitters/settings.py
+++ pawsitters/settings.py
@@ -77,7 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'cloudinary',
+    # 'cloudinary',
     # 'django.contrib.staticfiles',
     # 'cloudinary_storage',
 ]
